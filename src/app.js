b232e533c6aa2d623dea020a27cbf37e58de963d
Ivanko Perišić <info@studioperisic.com>
2019-02-20 20:28:44 +0100

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -18,9 +18,9 @@ store.subscribe(() => {
     console.log(visibleExpenses)
 })
 
-store.dispatch (addExpense({description: 'Water bill', amount: 4500}))
+/* store.dispatch (addExpense({description: 'Water bill', amount: 4500}))
 store.dispatch (addExpense({description: 'Gas bill', createdAt: 1000}))
-store.dispatch (addExpense({description: 'Rent', amount: 109500}))
+store.dispatch (addExpense({description: 'Rent', amount: 109500})) */
 
 //store.dispatch (removeExpense({id: expenseOne.expense.id}))
 //store.dispatch (editExpense(expenseTwo.expense.id,{amount: 500}))
