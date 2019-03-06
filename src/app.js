62bb527ccd332997a4c8d6a0920d7f19b0a26a7e
Ivanko Perišić <info@studioperisic.com>
2019-03-06 20:10:33 +0100

    Get firebase expenses on start

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import { addExpense } from './actions/expenses'
+import { startSetExpenses } from './actions/expenses'
 import { setTextFilter } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
@@ -44,7 +44,13 @@ store.dispatch (addExpense({description: 'Rent', amount: 109500})) */
 
 //---------------------
 
-ReactDOM.render(jsx, document.getElementById('app')); 
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app')); 
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app')); 
+})
+
+
 
 //--------------------------------------
 //HIGHER ORDER COMPONENT (HOC)
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,8 +1,16 @@
-import { startAddExpense, addExpense, removeExpense, editExpense } from '../../actions/expenses.js'
+import { startAddExpense, addExpense, removeExpense, editExpense, setExpenses, startSetExpenses } from '../../actions/expenses.js'
 import expenses from '../fictures/expenses'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import database from '../../firebase/firebase'
+
+beforeEach((done) => {
+    const expensesData = {}
+    expenses.forEach(({ id, description, note, amount, createdAt }) => {
+        expensesData[id] = { description, note, amount, createdAt}
+    })
+    database.ref('expenses').set(expensesData).then(() => done())
+})
 
 test('removeExpense', () => {
     const result = removeExpense({id: '123abc'});
@@ -108,3 +116,23 @@ test('should add expense with defaults to database and store', (done) => {
     }
     });
   }); */
+
+  test ('should set up expense action object with data', () => {
+    const action = setExpenses(expenses)
+    expect (action).toEqual({
+        type: 'SET_EXPENSES',
+        expenses
+    })
+  })
+
+  test ('Should fetch the expenses from firebase', (done) => {
+      const store = createMockStore({})
+      store.dispatch(startSetExpenses()).then(() => {
+      const actions = store.getActions()
+      expect(actions[0]).toEqual({
+          type: 'SET_EXPENSES',
+          expenses
+      })
+      done()
+    })
+  })
