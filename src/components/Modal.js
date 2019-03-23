d04231fbf245f942ae30260651d1dfdecceae11f
Ivanko Perišić <info@studioperisic.com>
2019-03-23 10:16:12 +0100

    Added dialog on remove and hidden expenses in summary

---
+++ src/components/Modal.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Modal from 'react-modal'
+
+export const OptionModal = (props) => (
+
+  <Modal
+    isOpen={props.showModal}
+    contentLabel="Confirm deletion"
+    ariaHideApp={false}
+    className='modal'
+  >
+    <div>
+      <p>Do you want to delete this expense?</p>
+      <button className="button-blue button-modal" onClick={props.onRemove}>Remove Expense</button>
+      <button className="button-blue button-modal" onClick={props.handleCloseModal}>Close</button>
+    </div>
+  </Modal>
+)
